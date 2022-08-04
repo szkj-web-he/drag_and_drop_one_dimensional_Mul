7f8e96a40ee94cebefe1c0e1862ca66bcc206457
szkj-web-he <xuejie.he@datareachable.com>
2022-08-04 10:29:28 +0800

    修改canvas的渐变

--- src/itemFrame.tsx
+++ src/itemFrame.tsx
@@ -1,7 +1,7 @@
 /* <------------------------------------ **** DEPENDENCE IMPORT START **** ------------------------------------ */
 /** This section will include all the necessary dependence for this tsx file */
-import React, { useRef } from "react";
-import { useLayoutEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { drawRoundRect } from "./unit";
 /* <------------------------------------ **** DEPENDENCE IMPORT END **** ------------------------------------ */
 /* <------------------------------------ **** INTERFACE START **** ------------------------------------ */
 /** This section will include all the interface for this tsx file */
@@ -14,95 +14,50 @@ interface TempProps {
 const Temp: React.FC<TempProps> = ({ style, className }) => {
     /* <------------------------------------ **** STATE START **** ------------------------------------ */
     /************* This section will include this component HOOK function *************/
-    // const [el, setEl] = useState<HTMLCanvasElement | null>(null);
+    const [el, setEl] = useState<HTMLCanvasElement | null>(null);
 
-    const ref = useRef<HTMLCanvasElement | null>(null);
+    const timer = useRef<number>();
+
     /* <------------------------------------ **** STATE END **** ------------------------------------ */
     /* <------------------------------------ **** PARAMETER START **** ------------------------------------ */
     /************* This section will include this component parameter *************/
-    useLayoutEffect(() => {
-        const drawRoundRectPath = (
-            ctx: CanvasRenderingContext2D,
-            width: number,
-            height: number,
-            x: number,
-            y: number,
-        ) => {
-            ctx.beginPath();
-
-            const r = 6;
-            //右下角
-            ctx.arc(width - r, height - r, r, 0, Math.PI / 2);
-            ctx.lineTo(r + x, height);
-
-            //左下角
-            ctx.arc(r + x, height - r, r, Math.PI / 2, Math.PI);
-            ctx.lineTo(x, r + y);
-
-            //左上角
-            ctx.arc(r + x, r + y, r, Math.PI, (Math.PI / 2) * 3);
-            ctx.lineTo(width - r, y);
-
-            //右上角
-            ctx.arc(width - r, r + y, r, (Math.PI / 2) * 3, Math.PI * 2);
-            ctx.lineTo(width, height - r);
-
-            ctx.closePath();
-        };
-
+    useEffect(() => {
         const fn = () => {
-            const node = ref.current;
-            if (!node) {
-                return;
-            }
-            const parent = node.parentElement;
-
-            if (parent instanceof HTMLElement) {
-                const width = parent.offsetWidth ?? 52;
-                const height = parent.offsetHeight ?? 37;
-
-                const ctx = node.getContext("2d");
-                if (!ctx) {
+            timer.current && window.clearTimeout(timer.current);
+            timer.current = window.setTimeout(() => {
+                const node = el;
+                timer.current = undefined;
+                if (!node) {
                     return;
                 }
-                ctx.clearRect(0, 0, width, height);
 
-                node.width = width;
-                node.height = height;
-
-                drawRoundRectPath(ctx, width - 1, height - 1, 1, 1);
-                ctx.save();
-
-                const bg = ctx.createLinearGradient(-14, -12.5, width, height - 4);
-                bg.addColorStop(0, "rgba(87,241,241,0.36)");
-                bg.addColorStop(1, "rgba(0,69,166,0.4)");
-                ctx.fillStyle = bg;
-                ctx.fill();
-
-                const strokeStyle = ctx.createLinearGradient(4.5, -3.5, width + 13, height + 11.5);
-                strokeStyle.addColorStop(0, "#57F1F1");
-                strokeStyle.addColorStop(1, "#007EFE");
-                ctx.restore();
-                ctx.strokeStyle = strokeStyle;
-
-                ctx.lineWidth = 0.6;
-
-                ctx.stroke();
-            }
+                drawRoundRect(node);
+            });
         };
-        fn();
         window.addEventListener("resize", fn);
         return () => {
             window.removeEventListener("resize", fn);
+            timer.current && window.clearTimeout(timer.current);
         };
-    }, []);
+    }, [el]);
 
     /* <------------------------------------ **** PARAMETER END **** ------------------------------------ */
     /* <------------------------------------ **** FUNCTION START **** ------------------------------------ */
     /************* This section will include this component general function *************/
+    timer.current && window.clearTimeout(timer.current);
+    timer.current = window.setTimeout(() => {
+        const node = el;
+        timer.current = undefined;
+        if (!node) {
+            return;
+        }
+
+        drawRoundRect(node);
+    });
+
     /* <------------------------------------ **** FUNCTION END **** ------------------------------------ */
 
-    return <canvas className={className} style={style} ref={ref} />;
+    return <canvas className={className} style={style} ref={(el) => setEl(el)} />;
 };
 /* <------------------------------------ **** FUNCTION COMPONENT END **** ------------------------------------ */
 export default Temp;
